5da4f7d9a21d1d901bf30eecc959ad754e28d45b
Scarcestar-XieCY <402600293@qq.com>
2023-03-25 19:41:55 +0800

    fix eval in main

--- matchnet/code/eval_form2fit.py
+++ matchnet/code/eval_form2fit.py
@@ -4,7 +4,7 @@
 import argparse
 import os
 import pickle
-
+import json
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -30,6 +30,23 @@ KIT_IDXS_ALL = "kit_idxs_all"
 OBJ_IDXS_ALL = "obj_idxs_all"
 COORD_NAMES = ["obj_uniform","obj_ccircle"]
 NUM_ROT = 20
+
+
+class NumpyEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.ndarray):
+        return obj.tolist()
+    return super(NumpyEncoder, self).default(obj)
+
+
+def dump_json(pred_dict, dump_path):
+    pred_dict
+    json_str = json.dumps(pred_dict, indent=4,cls=NumpyEncoder)
+    with open(dump_path, 'w') as json_file:
+        json_file.write(json_str)
+    logger.warning("Saved json at %s",dump_path)
 
 def get_circle_point(shape, uv_center, radius):
     """Get all point of one circle."""
@@ -186,27 +203,57 @@ def validation_correspondence(dloader,model,device, num_subsample=None,interval=
             prec_dict[coord_name]["pred_seq"].append(pred_best_rot) # acc of one obj
             prec_dict[coord_name]["gt_seq"].append(correct_rot) # acc of one obj
             
-            if pred_best_rot == correct_rot:
-                prec_dict[coord_name]["acc"] += 1
+            # if pred_best_rot == correct_rot:
+            #     prec_dict[coord_name]["acc"] += 1
 
     for coord_name in COORD_NAMES:        
         # prec_dict[coord_name]["acc"] = prec_dict[coord_name]["acc"]* interval/ len(dloader)
         prec_dict[coord_name]["acc_one_obj"] = np.mean(prec_dict[coord_name]["acc_one_obj"]) if not debug else prec_dict[coord_name]["acc_one_obj"]
 
-    prec_dict[coord_name]["tp_seq"] = {}
-    prec_dict[coord_name]["fp_seq"] = {}
-    prec_dict[coord_name]["fn_seq"] = {}
-    prec_dict[coord_name]["angle_p"] = []
-    prec_dict[coord_name]["angle_r"] = []
-    for i in range(NUM_ROT):
-        prec_dict[coord_name]["tp_seq"][i] = np.array(prec_dict[coord_name]["pred_seq"]) == i  & np.array(prec_dict[coord_name]["gt_seq"]) == i
-        prec_dict[coord_name]["fp_seq"][i] = np.array(prec_dict[coord_name]["pred_seq"]) == i  & np.array(prec_dict[coord_name]["gt_seq"]) != i
-        prec_dict[coord_name]["fn_seq"][i] = np.array(prec_dict[coord_name]["pred_seq"]) != i  & np.array(prec_dict[coord_name]["gt_seq"]) == i
-    for i in range(NUM_ROT):
-        prec_dict[coord_name]["angle_p"].append(prec_dict[coord_name]["tp_seq"][i] / (prec_dict[coord_name]["tp_seq"][i] + prec_dict[coord_name]["fp_seq"][i]))
-        prec_dict[coord_name]["angle_r"].append(prec_dict[coord_name]["tp_seq"][i] / (prec_dict[coord_name]["tp_seq"][i] + prec_dict[coord_name]["fn_seq"][i]))
-    prec_dict[coord_name]["angle_p_mean"] = np.mean(prec_dict[coord_name]["angle_p"])
-    prec_dict[coord_name]["angle_r_mean"] = np.mean(prec_dict[coord_name]["angle_r"])
+        prec_dict[coord_name]["tp_seq"] = {}
+        prec_dict[coord_name]["fp_seq"] = {}
+        prec_dict[coord_name]["fn_seq"] = {}
+        prec_dict[coord_name]["angle_p"] = []
+        prec_dict[coord_name]["angle_r"] = []
+        prec_dict[coord_name]["tp"] = []
+        prec_dict[coord_name]["fp"] = []
+        prec_dict[coord_name]["fn"] = []
+        for i in range(NUM_ROT):
+            prec_dict[coord_name]["tp_seq"][i] = np.logical_and(np.array(prec_dict[coord_name]["pred_seq"]) == i,np.array(prec_dict[coord_name]["gt_seq"]) == i)
+            prec_dict[coord_name]["fp_seq"][i] = np.logical_and(np.array(prec_dict[coord_name]["pred_seq"]) == i, np.array(prec_dict[coord_name]["gt_seq"]) != i)
+            prec_dict[coord_name]["fn_seq"][i] = np.logical_and(np.array(prec_dict[coord_name]["pred_seq"]) != i, np.array(prec_dict[coord_name]["gt_seq"]) == i)
+        valid_mask_pre = []
+        valid_mask_rec = []
+        for i in range(NUM_ROT):
+            
+            prec_dict[coord_name]["tp"].append(np.sum(prec_dict[coord_name]["tp_seq"][i]))
+            prec_dict[coord_name]["fp"].append(np.sum(prec_dict[coord_name]["fp_seq"][i]))
+            prec_dict[coord_name]["fn"].append(np.sum(prec_dict[coord_name]["fn_seq"][i]))
+            if (prec_dict[coord_name]["tp"][i] + prec_dict[coord_name]["fp"][i]) != 0:
+                pre = prec_dict[coord_name]["tp"][i] / (prec_dict[coord_name]["tp"][i] + prec_dict[coord_name]["fp"][i])
+                valid_mask_pre.append(True)
+            else:
+                pre = 0
+                valid_mask_pre.append(False)
+
+            if (prec_dict[coord_name]["tp"][i] + prec_dict[coord_name]["fn"][i]) != 0:
+                rec = prec_dict[coord_name]["tp"][i] / (prec_dict[coord_name]["tp"][i] + prec_dict[coord_name]["fn"][i])
+                valid_mask_rec.append(True)
+            else:
+                rec = 0
+                valid_mask_rec.append(False)
+
+            prec_dict[coord_name]["angle_p"].append(pre)
+            prec_dict[coord_name]["angle_r"].append(rec)
+
+        valid_mask_pre = np.array(valid_mask_pre)
+        valid_mask_rec = np.array(valid_mask_rec)
+        prec_dict[coord_name]["angle_p_mean"] = np.mean(np.array(prec_dict[coord_name]["angle_p"])[valid_mask_pre])
+        prec_dict[coord_name]["angle_r_mean"] = np.mean(np.array(prec_dict[coord_name]["angle_r"])[valid_mask_rec])
+
+        prec_dict[coord_name].pop("tp_seq")
+        prec_dict[coord_name].pop("fp_seq")
+        prec_dict[coord_name].pop("fn_seq")
     # ap = np.mean(prec_list)
     return prec_dict
 
@@ -242,11 +289,6 @@ def main(args):
     )
 
 
-    color_mean = dloader.dataset.c_mean
-    color_std = dloader.dataset.c_std
-    depth_mean = dloader.dataset.d_mean
-    depth_std = dloader.dataset.d_std
-
     # load model
     model = CorrespondenceNet(num_channels, 64, 20).to(device)
     # state_dict = torch.load(os.path.join(config.weights_dir, "matching", args.foldername + ".tar"), map_location=device)
@@ -254,154 +296,20 @@ def main(args):
     model.load_state_dict(state_dict["model"])
     model.eval()
 
-    estimated_poses = []
-    correct = 0
 
     pred_dict = validation_correspondence(dloader,model,device,args.subsample,args.interval)
-    recursive_read(pred_dict,"")
-    # for idx, (imgs, labels, center) in enumerate(tqdm(dloader)):
-    #     # print("{}/{}".format(idx+1, len(dloader)))
-    #     if idx % 10 != 0:
-    #         continue
-    #     imgs, labels = imgs.to(device), labels.to(device)
-
-    #     # remove padding from labels
-    #     label = labels[0]
-    #     mask = torch.all(label == torch.LongTensor([999]).repeat(6).to(device), dim=1)
-    #     label = label[~mask]
-
-    #     # extract correspondences from label
-    #     source_idxs = label[:, 0:2]
-    #     target_idxs = label[:, 2:4]
-    #     rot_idx = label[:, 4]
-    #     is_match = label[:, 5]
-    #     correct_rot = rot_idx[0]
-    #     mask = (is_match == 1) & (rot_idx == correct_rot)
-    #     kit_idxs = source_idxs[mask]
-    #     obj_idxs = target_idxs[mask]
-    #     kit_idxs_all = kit_idxs.clone()
-    #     obj_idxs_all = obj_idxs.clone()
-    #     if args.subsample is not None:
-    #         kit_idxs = kit_idxs[::int(args.subsample)]
-    #         obj_idxs = obj_idxs[::int(args.subsample)]
-
-    #     H, W = imgs.shape[2:]
-
-    #     # compute kit and object descriptor maps
-    #     with torch.no_grad():
-    #         outs_s, outs_t = model(imgs, *center[0])
-    #     out_s = outs_s[0]
-    #     D, H, W = outs_t.shape[1:]
-    #     out_t = outs_t[0:1]
-    #     out_t_flat = out_t.view(1, D, H * W).permute(0, 2, 1)
-
-    #     color_kit = ml.tensor2ndarray(imgs[:, 0, :], [color_mean, color_std], True).squeeze()
-    #     color_obj = ml.tensor2ndarray(imgs[:, 2, :], [color_mean, color_std], True).squeeze()
-    #     depth_kit = ml.tensor2ndarray(imgs[:, 1, :], [depth_mean, depth_std], False).squeeze()
-    #     depth_obj = ml.tensor2ndarray(imgs[:, 3, :], [depth_mean, depth_std], False).squeeze()
-    #     if args.debug:
-    #         kit_idxs_np = kit_idxs_all.detach().cpu().numpy().squeeze()
-    #         obj_idxs_np = obj_idxs_all.detach().cpu().numpy().squeeze()
-    #         fig, axes = plt.subplots(1, 2)
-    #         color_kit_r = misc.rotate_img(color_kit, -(360/20)*correct_rot, center=(center[0][1], center[0][0]))
-    #         axes[0].imshow(color_kit_r)
-    #         axes[0].scatter(kit_idxs_np[:, 1], kit_idxs_np[:, 0], c='r')
-    #         axes[1].imshow(color_obj)
-    #         axes[1].scatter(obj_idxs_np[:, 1], obj_idxs_np[:, 0], c='b')
-    #         for ax in axes:
-    #             ax.axis('off')
-    #         plt.show()
-
-    #     # loop through ground truth correspondences
-    #     obj_uvs = []
-    #     predicted_kit_uvs = []
-    #     rotations = []
-        
-    #     for corr_idx, (u, v) in enumerate(obj_idxs):
-    #         idx_flat = u*W + v
-    #         target_descriptor = torch.index_select(out_t_flat, 1, idx_flat).squeeze(0)
-    #         outs_s_flat = out_s.view(20, out_s.shape[1], H*W)
-    #         target_descriptor = target_descriptor.unsqueeze(0).repeat(20, H*W, 1).permute(0, 2, 1)
-    #         diff = outs_s_flat - target_descriptor
-    #         l2_dist = diff.pow(2).sum(1).sqrt()
-    #         heatmaps = l2_dist.view(l2_dist.shape[0], H, W).cpu().numpy()
-    #         predicted_best_idx = l2_dist.min(dim=1)[0].argmin()
-    #         rotations.append(predicted_best_idx.item())
-    #         # if predicted_best_idx == correct_rot:
-    #         #     correct += 1
-    #         min_val = heatmaps[predicted_best_idx].argmin()
-    #         u_min, v_min = np.unravel_index(min_val, (H, W))
-    #         predicted_kit_uvs.append([u_min.item(), v_min.item()])
-    #         obj_uvs.append([u.item(), v.item()])
-        
-
-    #     # compute rotation majority
-    #     pred_best_rot = mode(rotations,axis=None,keepdims=False)[0]
-    #     if pred_best_rot == correct_rot.item():
-    #         correct += 1
-
-    #     # eliminate correspondences with rotation different than mode
-    #     select_idxs = np.array(rotations) == pred_best_rot
-    #     predicted_kit_uvs = np.array(predicted_kit_uvs)[select_idxs]
-    #     obj_uvs = np.array(obj_uvs)[select_idxs]
-
-    #     # use predicted correspondences to estimate affine transformation
-    #     src_pts = np.array(obj_uvs)[:, [1, 0]]
-    #     dst_pts = np.array(predicted_kit_uvs)
-    #     dst_pts = misc.rotate_uv(dst_pts, (360/20)*pred_best_rot, H, W, cxcy=center[0])[:, [1, 0]]
-
-    #     # compose transform
-    #     zs = depth_obj[src_pts[:, 1], src_pts[:, 0]].reshape(-1, 1)
-    #     src_xyz = np.hstack([src_pts, zs])
-    #     # src_xyz[:, 0] = (src_xyz[:, 0] * config.HEIGHTMAP_RES) + config.VIEW_BOUNDS[0, 0]
-    #     # src_xyz[:, 1] = (src_xyz[:, 1] * config.HEIGHTMAP_RES) + config.VIEW_BOUNDS[1, 0]
-    #     zs = depth_kit[dst_pts[:, 1], dst_pts[:, 0]].reshape(-1, 1)
-    #     dst_pts[:, 0] += W
-    #     dst_xyz = np.hstack([dst_pts, zs])
-    #     # dst_xyz[:, 0] = (dst_xyz[:, 0] * config.HEIGHTMAP_RES) + config.VIEW_BOUNDS[0, 0]
-    #     # dst_xyz[:, 1] = (dst_xyz[:, 1] * config.HEIGHTMAP_RES) + config.VIEW_BOUNDS[1, 0]
-    #     m1 = np.eye(4)
-    #     dst_xyz[:, 2] = src_xyz[:, 2]
-    #     m1[:3, 3] = np.mean(dst_xyz, axis=0) - np.mean(src_xyz, axis=0)
-    #     m2 = np.eye(4)
-    #     m2[:3, 3] = -np.mean(dst_xyz, axis=0)
-    #     m3 = np.eye(4)
-    #     m3[:3, :3] = gen_rot_mtx_anticlockwise(np.radians(-(360/20)*pred_best_rot))
-    #     m4 = np.eye(4)
-    #     m4[:3, 3] = np.mean(dst_xyz, axis=0)
-    #     estimated_pose = m4 @ m3 @ m2 @ m1
-    #     estimated_poses.append(estimated_pose)
-
-    #     # plot
-    #     if args.debug:
-    #         img = np.zeros((H, W*2))
-    #         img[:, :W] = color_obj
-    #         img[:, W:] = color_kit
-    #         zs = depth_obj[obj_idxs_np[:, 0], obj_idxs_np[:, 1]].reshape(-1, 1)
-    #         mask_xyz = np.hstack([obj_idxs_np, zs])
-    #         mask_xyz[:, [0, 1]] = mask_xyz[:, [1, 0]]
-    #         # mask_xyz[:, 0] = (mask_xyz[:, 0] * config.HEIGHTMAP_RES) + config.VIEW_BOUNDS[0, 0]
-    #         # mask_xyz[:, 1] = (mask_xyz[:, 1] * config.HEIGHTMAP_RES) + config.VIEW_BOUNDS[1, 0]
-    #         mask_xyz = transform_xyz(mask_xyz, estimated_pose)
-    #         # mask_xyz[:, 0] = (mask_xyz[:, 0] - config.VIEW_BOUNDS[0, 0]) / config.HEIGHTMAP_RES
-    #         # mask_xyz[:, 1] = (mask_xyz[:, 1] - config.VIEW_BOUNDS[1, 0]) / config.HEIGHTMAP_RES
-    #         hole_idxs = mask_xyz[:, [1, 0]]
-    #         plt.imshow(img)
-    #         plt.scatter(hole_idxs[:, 1], hole_idxs[:, 0])
-    #         plt.show()
-    # print("acc: {}".format(correct / len(dloader)))
-    # with open(os.path.join(save_dir, "{}_poses.pkl".format(args.foldername)), "wb") as fp:
-    #     pickle.dump(estimated_poses, fp)
-    # logger.warning("Save pkl at %s",os.path.join(save_dir, "{}_poses.pkl".format(args.foldername)))
+    # recursive_read(pred_dict,"")
+    dump_json(pred_dict, args.dump_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate Form2Fit Matching Module on Benchmark")
-    parser.add_argument("--foldername", type=str, help="The name of the dataset.",default="../datasets_mix0128")
-    parser.add_argument("--model_path", type=str, help="Path to model.",default="matchnet/code/ml/savedmodel/mix0128_2/corrs_epoch199.pth")
+    parser.add_argument("--foldername", type=str, help="The name of the dataset.",default="../datasets_mix0128")# datasets_mix0128  datasets_bear
+    parser.add_argument("--model_path", type=str, help="Path to model.",default="matchnet/code/ml/savedmodel/mix0128_2/corrs_epoch99.pth")
+    parser.add_argument("--dump_path", type=str, help="Path to dump eval results.",default="eval/test_json.json")
     parser.add_argument("--dtype", type=str, default="train")
-    parser.add_argument("--subsample", type=int, default=16)
-    parser.add_argument("--interval", type=int, default=20)
+    parser.add_argument("--subsample", type=int, default=5)
+    parser.add_argument("--interval", type=int, default=5)
     parser.add_argument("--debug", type=lambda s: s.lower() in ["1", "true"], default=False)
     args, unparsed = parser.parse_known_args()
     main(args)
